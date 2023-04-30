89d25583dba79290f92e4b466cd26f9752dc8a31
ot07 <ot07neoki@gmail.com>
2023-04-30 23:36:44 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import {basename, resolve} from 'path'
+import {defineConfig} from 'vite'
+import dts from 'vite-plugin-dts'
+import packageJson from './package.json'
+
+const getPackageName = () => {
+  return basename(packageJson.name)
+}
+
+const getPackageNameCamelCase = () => {
+  try {
+    return getPackageName().replace(/-./g, (char) => char[1].toUpperCase())
+  } catch (err) {
+    throw new Error('Name property in package.json is missing.')
+  }
+}
+
+module.exports = defineConfig({
+  plugins: [dts({insertTypesEntry: true})],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: getPackageNameCamelCase(),
+      formats: ['es'],
+      fileName: getPackageName(),
+    },
+  },
+})
+
